006eb9d2893a678be317d159265c977d59f6b971
AntufeevMaksim <antufeevmaksim9@gmail.com>
2026-03-11 22:07:27 +0700

    Version without field sending

--- lab4/main.c
+++ lab4/main.c
@@ -31,9 +31,8 @@ void write_info(int num_proc, double time)
     fprintf(f, "Num proc: %d\n Total time: %lf\n", num_proc, time);
 }
 
-void write_mat(bool *mat, int h, int w)
+void write_mat(FILE *f, bool *mat, int h, int w)
 {
-    FILE *f = fopen("output.txt", "w");
     if (f == NULL)
     {
         perror("fopen");
@@ -49,8 +48,6 @@ void write_mat(bool *mat, int h, int w)
         }
         fputc('\n', f);
     }
-
-    fclose(f);
 }
 
 static inline void swap(bool **a, bool **b)
@@ -77,101 +74,35 @@ static inline int calc_real_h(int h, int coord, ProgramData *data)
     return h;
 }
 
-bool *init_empty(int N)
+void init_empty(bool *field, int st_h, int h, int w)
 {
-    bool *field = (bool *)malloc(N * N * sizeof(bool));
-    for (int y = 0; y < N; ++y)
+    for (int y = 0; y < h; ++y)
     {
-        for (int x = 0; x < N; ++x)
+        for (int x = 0; x < w; ++x)
         {
-            field[N * y + x] = 0;
+            field[w * y + x] = 0;
         }
     }
-    return field;
 }
 
-bool *init_glider(int N)
+void init_line(bool *field, int st_h, int h, int w)
 {
-    bool *field = init_empty(N);
+    init_empty(field, st_h, h, w);
 
-    int x = 1;
-    int y = 1;
+    int x = w / 2;
 
-    field[y * N + (x + 1)] = true;
-    field[(y + 1) * N + (x + 2)] = true;
-    field[(y + 2) * N + x] = true;
-    field[(y + 2) * N + (x + 1)] = true;
-    field[(y + 2) * N + (x + 2)] = true;
-
-    return field;
-}
-
-bool *init_line(int N)
-{
-    bool *field = init_empty(N);
-
-    int x = N / 2;
-
-    for (int y = 0; y < N; ++y)
+    for (int y = 0; y < h; ++y)
     {
-        field[y * N + x] = 1;
+        field[y * w + x] = 1;
     }
-    return field;
 }
 
-bool *init_blinker(int N)
+void init_field(char *arg, bool *field, int st_h, int h, int w)
 {
-    bool *field = init_empty(N);
-
-    int cx = N / 2;
-    int cy = N / 2;
-
-    field[cy * N + (cx - 1)] = true;
-    field[cy * N + cx] = true;
-    field[cy * N + (cx + 1)] = true;
-
-    return field;
-}
-
-bool *init_random(int N, unsigned int seed)
-{
-    srand(seed);
-
-    bool *field = (bool *)malloc(N * N * sizeof(bool));
-    if (field == NULL)
-        return NULL;
-
-    for (int y = 0; y < N; ++y)
-    {
-        for (int x = 0; x < N; ++x)
-        {
-            field[N * y + x] = rand() % 2;
-        }
-    }
-
-    return field;
-}
-
-bool *init_field(char *arg, int N)
-{
-    bool *field;
     if (strcmp(arg, "line") == 0)
     {
-        field = init_line(N);
+        init_line(field, st_h, h, w);
     }
-    else if (strcmp(arg, "glider") == 0)
-    {
-        field = init_glider(N);
-    }
-    else if (strcmp(arg, "blinker") == 0)
-    {
-        field = init_blinker(N);
-    }
-    else if (strcmp(arg, "random") == 0)
-    {
-        field = init_random(N, 137);
-    }
-    return field;
 }
 
 bool calc_next_state(bool *field, int x, int y, int local_sx, int local_sy)
@@ -209,26 +140,6 @@ bool calc_next_state(bool *field, int x, int y, int local_sx, int local_sy)
     return new_state;
 }
 
-void send_field(bool *field, bool *local_field, int coord, ProgramData *data, MPI_Comm comm_1d)
-{
-    int *counts = (int *)malloc(data->size * sizeof(int));
-    int *displs = (int *)malloc(data->size * sizeof(int));
-
-    int disp = 0;
-    for (int i = 0; i < data->size; ++i)
-    {
-        displs[i] = disp;
-
-        counts[i] = calc_h(i, data);
-        counts[i] = calc_real_h(counts[i], i, data) * data->N;
-
-        disp = (calc_start_h(i + 1, data) - 1) * data->N;
-    }
-
-    int recv_count = calc_real_h(calc_h(coord, data), coord, data) * data->N;
-    MPI_Scatterv(field, counts, displs, MPI_C_BOOL, local_field, recv_count, MPI_C_BOOL, 0, comm_1d);
-}
-
 bool *run_game(bool *local_field, int iters, ProgramData *data, int coord, MPI_Comm comm_1d)
 {
     const int SEND_UP = 0;
@@ -244,8 +155,8 @@ bool *run_game(bool *local_field, int iters, ProgramData *data, int coord, MPI_C
 
     for (int i = 0; i < iters; ++i)
     {
-        MPE_Log_event(EVENT_ITER_START, 0, NULL);
-        MPE_Log_event(EVENT_COMP_START, 0, NULL);
+        // MPE_Log_event(EVENT_ITER_START, 0, NULL);
+        // MPE_Log_event(EVENT_COMP_START, 0, NULL);
 
         if (coord != 0)
         {
@@ -267,7 +178,7 @@ bool *run_game(bool *local_field, int iters, ProgramData *data, int coord, MPI_C
             MPI_Isend(tmp + data->N * (h - 2), data->N, MPI_C_BOOL, coord + 1, SEND_DOWN, comm_1d, &send_down);
             MPI_Irecv(tmp + data->N * (h - 1), data->N, MPI_C_BOOL, coord + 1, RECV_DOWN, comm_1d, &recv_down);
         }
-        MPE_Log_event(EVENT_COMM_START, 0, NULL);
+        // MPE_Log_event(EVENT_COMM_START, 0, NULL);
 
         int st = coord == 0 ? 0 : 1;
         for (int y = st; y < proc_h + st; ++y)
@@ -277,7 +188,7 @@ bool *run_game(bool *local_field, int iters, ProgramData *data, int coord, MPI_C
                 tmp[data->N * y + x] = calc_next_state(local_field, x, y, data->N, h);
             }
         }
-        MPE_Log_event(EVENT_COMP_END, 0, NULL);
+        // MPE_Log_event(EVENT_COMP_END, 0, NULL);
 
         if (coord != 0)
         {
@@ -289,29 +200,71 @@ bool *run_game(bool *local_field, int iters, ProgramData *data, int coord, MPI_C
             MPI_Wait(&send_down, MPI_STATUS_IGNORE);
             MPI_Wait(&recv_down, MPI_STATUS_IGNORE);
         }
-        MPE_Log_event(EVENT_COMM_END, 0, NULL);
+        // MPE_Log_event(EVENT_COMM_END, 0, NULL);
         swap(&tmp, &local_field);
 
-        MPE_Log_event(EVENT_ITER_END, 0, NULL);
+        // MPE_Log_event(EVENT_ITER_END, 0, NULL);
     }
     free(tmp);
     return local_field;
 }
 
-void recv_field(bool *field, bool *local_field, int coord, ProgramData *data, MPI_Comm comm_1d)
+void recv_field(bool *field, int coord, ProgramData *data, MPI_Comm comm_1d)
 {
-    int *recv_counts = (int *)malloc(data->size * sizeof(int));
-    int *displs = (int *)malloc(data->size * sizeof(int));
 
-    for (int i = 0; i < data->size; ++i)
+    if (coord == 0)
     {
-        displs[i] = calc_start_h(i, data) * data->N;
-        recv_counts[i] = calc_h(i, data) * data->N;
+        FILE *f = fopen("result.txt", "w");
+        if (f == NULL)
+        {
+            perror("fopen");
+            return;
+        }
+
+        int h = calc_h(coord, data);
+        for (int y = 0; y < h; ++y)
+        {
+            for (int x = 0; x < data->N; ++x)
+            {
+                char c = field[y * data->N + x] ? '#' : '.';
+                fputc(c, f);
+            }
+            fputc('\n', f);
+        }
+        fclose(f);
     }
 
-    int send_count = calc_h(coord, data) * data->N;
-    int start = (coord == 0) ? 0 : data->N;
-    MPI_Gatherv(local_field + start, send_count, MPI_C_BOOL, field, recv_counts, displs, MPI_C_BOOL, 0, comm_1d);
+    for (int i = 1; i < data->size; ++i)
+    {
+        if (coord == i)
+        {
+            MPI_Recv(NULL, 0, MPI_INT, i - 1, 0,
+                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            FILE *f = fopen("result.txt", "a");
+            if (f == NULL)
+            {
+                perror("fopen");
+                return;
+            }
+
+            int h = calc_h(coord, data);
+            for (int y = 0; y < h; ++y)
+            {
+                for (int x = 0; x < data->N; ++x)
+                {
+                    char c = field[y * data->N + x] ? '#' : '.';
+                    fputc(c, f);
+                }
+                fputc('\n', f);
+            }
+            fclose(f);
+        }
+        else if (coord == i - 1)
+        {
+            MPI_Send(NULL, 0, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -323,17 +276,17 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPE_Log_get_state_eventIDs(&EVENT_COMP_START, &EVENT_COMP_END);
-    MPE_Describe_state(EVENT_COMP_START, EVENT_COMP_END,
-                       "COMPUTE", "red");
+    // MPE_Log_get_state_eventIDs(&EVENT_COMP_START, &EVENT_COMP_END);
+    // MPE_Describe_state(EVENT_COMP_START, EVENT_COMP_END,
+    //                    "COMPUTE", "red");
 
-    MPE_Log_get_state_eventIDs(&EVENT_COMM_START, &EVENT_COMM_END);
-    MPE_Describe_state(EVENT_COMM_START, EVENT_COMM_END,
-                       "COMM", "blue");
+    // MPE_Log_get_state_eventIDs(&EVENT_COMM_START, &EVENT_COMM_END);
+    // MPE_Describe_state(EVENT_COMM_START, EVENT_COMM_END,
+    //                    "COMM", "blue");
 
-    MPE_Log_get_state_eventIDs(&EVENT_ITER_START, &EVENT_ITER_END);
-    MPE_Describe_state(EVENT_ITER_START, EVENT_ITER_END,
-                       "ITER", "green");
+    // MPE_Log_get_state_eventIDs(&EVENT_ITER_START, &EVENT_ITER_END);
+    // MPE_Describe_state(EVENT_ITER_START, EVENT_ITER_END,
+    //                    "ITER", "green");
 
     if (argc != 4)
     {
@@ -368,30 +321,24 @@ int main(int argc, char **argv)
     int start_h = calc_start_h(coord, &data);
     int height = calc_h(coord, &data);
 
-    bool *field;
+    int h = calc_real_h(calc_h(coord, &data), coord, &data);
+    int st_h = calc_h(coord, &data);
+    bool *field = (bool *)malloc(h * N * sizeof(bool));
+    init_field(argv[1], field, st_h, h, N);
 
-    if (coord == 0)
-    {
-        field = init_field(argv[1], N);
-    }
     int local_size_y = height;
     local_size_y += (coord == 0 ? 0 : 1);
     local_size_y += (coord == size - 1 ? 0 : 1);
 
-    bool *local_field = (bool *)malloc(N * local_size_y * sizeof(bool));
+    field = run_game(field, iters, &data, coord, comm_1d);
 
-    send_field(field, local_field, coord, &data, comm_1d);
-
-    local_field = run_game(local_field, iters, &data, coord, comm_1d);
-
-    recv_field(field, local_field, coord, &data, comm_1d);
+    recv_field(field, coord, &data, comm_1d);
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = (end.tv_sec - start.tv_sec) +
                      (end.tv_nsec - start.tv_nsec) / 1e9;
     if (coord == 0)
     {
-        write_mat(field, N, N);
         write_info(size, elapsed);
     }
 
